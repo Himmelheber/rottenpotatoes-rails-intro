e0c0d8a5f77eb1bb7bc923261beaf5fa6c5c5a77
Himmelheber <dhimmelh@uccs.edu>
2019-02-26 02:34:09 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,11 @@
-class MoviesController < ApplicationController
-
-  def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
-  end
-
-  def show
+def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
   def index
+
     if params[:sort].nil? && params[:ratings].nil? &&
         (!session[:sort].nil? || !session[:ratings].nil?)
       redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
@@ -37,7 +32,7 @@ class MoviesController < ApplicationController
         @movies = Movie.find_all_by_rating(ratings)
       end
     else
-      @movies = Movie.find_or_create_by(ratings)
+      @movies = Movie.find_all_by_rating(ratings)
     end
 
     session[:sort] = @sort
@@ -49,7 +44,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = Movie.create!(movie_params)
+    @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
@@ -60,7 +55,7 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(movie_params)
+    @movie.update_attributes!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
   end
@@ -71,5 +66,3 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-
-end
